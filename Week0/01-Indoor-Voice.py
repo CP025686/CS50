c0cb961c7b678f23236b21b84bc0e6bf327c4c48
Lester Steenbok <steenbokdl@gmail.com>
2024-08-27 13:09:44 +0200

    Initial Commit

--- Week0/01-Indoor-Voice.py
+++ Week0/01-Indoor-Voice.py
@@ -2,3 +2,6 @@
 Implement a program in Python that prompts the user for input and then outputs that same input in lowercase.
 Punctuation and whitespace should be outputted unchanged.
 """
+
+text = input("ENTER WORD(S) IN ALL CAPS: ").lower()
+print(text)
